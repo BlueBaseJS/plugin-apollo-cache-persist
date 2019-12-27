f7f63f76fce0925679eaeb25f888540fb70afe26
Hashir Ch <info@iammhc.com>
2019-12-27 19:25:12 +0500

    fix(persist): Memory Remove After Boot

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,16 +1,17 @@
-// const { persistCache } = jest.genMockFromModule('apollo-cache-persist');
-
-jest.mock('apollo-cache-persist', () => {
-	return { persistCache: jest.fn() };
-});
-
 import 'cross-fetch/polyfill';
 
+// const { persistCache } = jest.genMockFromModule('apollo-cache-persist');
+// import { ApolloClient } from 'apollo-client';
 import { BlueBase } from '@bluebase/core';
 import BlueBasePluginApollo from '@bluebase/plugin-apollo';
 import { CachePersistor } from 'apollo-cache-persist';
 import Plugin from '../index';
 
+jest.mock('apollo-cache-persist', () => {
+	return { persistCache: jest.fn() };
+});
+
+const client = jest.fn();
 jest.mock('apollo-cache-persist', () => ({ CachePersistor: jest.fn() }));
 
 test('Plugin should be correctly registered', async () => {
@@ -18,7 +19,8 @@ test('Plugin should be correctly registered', async () => {
 
 	await BB.boot({ plugins: [BlueBasePluginApollo, Plugin] });
 	expect(BB.Plugins.has('@bluebase/plugin-apollo-cache-persist')).toBeTruthy();
+	BB.Filters.run('plugin.apollo.client', client);
 	await BB.reset();
 
-	expect(CachePersistor).toBeCalledTimes(1);
+	expect(CachePersistor).toBeCalledTimes(2);
 });
