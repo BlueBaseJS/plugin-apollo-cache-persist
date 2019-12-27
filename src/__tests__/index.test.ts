610be4a41306f657a6d564fa36d07361dea13be0
Hashir Ch <info@iammhc.com>
2019-12-27 22:33:00 +0500

    test(Apollo Cache Persist): filter plugin apollo client

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,25 +1,34 @@
 import 'cross-fetch/polyfill';
 
-// const { persistCache } = jest.genMockFromModule('apollo-cache-persist');
-// import { ApolloClient } from 'apollo-client';
+import { ApolloClient } from 'apollo-client';
 import { BlueBase } from '@bluebase/core';
 import BlueBasePluginApollo from '@bluebase/plugin-apollo';
 import { CachePersistor } from 'apollo-cache-persist';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import Plugin from '../index';
+import { createHttpLink } from 'apollo-link-http';
+
+// const { persistCache } = jest.genMockFromModule('apollo-cache-persist');
 
 jest.mock('apollo-cache-persist', () => {
 	return { persistCache: jest.fn() };
 });
 
-const client = jest.fn();
+// const client = { onClearStore: async () => jest.fn() };
 jest.mock('apollo-cache-persist', () => ({ CachePersistor: jest.fn() }));
-
+const httpLink = createHttpLink();
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+	cache,
+	link: httpLink,
+});
 test('Plugin should be correctly registered', async () => {
 	const BB = new BlueBase();
 
 	await BB.boot({ plugins: [BlueBasePluginApollo, Plugin] });
 	expect(BB.Plugins.has('@bluebase/plugin-apollo-cache-persist')).toBeTruthy();
-	BB.Filters.run('plugin.apollo.client', client);
+	await BB.Filters.run('plugin.apollo.client', client);
+	client.clearStore();
 	await BB.reset();
 
 	expect(CachePersistor).toBeCalledTimes(2);
